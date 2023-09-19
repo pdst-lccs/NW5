a5f171238521fc7bf2d866133096d039fb1a050b
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-09-19 18:31:09 +0100

    Update part2_ThonnyToThingSpeakTest.py

--- part2_ThonnyToThingSpeakTest.py
+++ part2_ThonnyToThingSpeakTest.py
@@ -6,6 +6,8 @@ import urllib.request
 
 number=input('Enter a number : ')
 number = str(number)
+
+#change the API key 
 b=urllib.request.urlopen('https://api.thingspeak.com/update?api_key=BIZPFWPP6YGOAVHR&field1='+number)
 print("\nYour number has been sent!")
 print(number)
